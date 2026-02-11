1f4ffaed45378aac0b6d46095197703cb9e14d91
Gustavo <gustavomffaria@example.com>
2026-02-11 16:01:54 -0400

    Deploy final com Neon Database

--- server/index.js
+++ server/index.js
@@ -58,10 +58,6 @@ const verificaToken = (req, res, next) => {
   }
 };
 
-router.get('/', (req, res) => {
-  res.send('Backend Funcionando - Rota Raiz');
-});
-
 router.post('/register', async (req, res) => {
   const { username, password } = req.body;
   try {
@@ -114,6 +110,10 @@ router.post('/logout', (req, res) => {
 
 router.get('/me', verificaToken, (req, res) => {
   res.json({ user: req.user });
+});
+
+router.get('/', (req, res) => {
+  res.send('API Online');
 });
 
 app.use('/api', router);
